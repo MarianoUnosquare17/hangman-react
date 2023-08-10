fe9ddabeecac5b8f69d5753663b34d9622884f1d
Mariano Estrada <mariano.estrada@vesta.io>
2023-08-10 13:16:03 -0600

    adelanto

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,79 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useRef } from 'react'
+import './App.css'
 
+function App () {
+  const [guess, setGuess] = useState<number>(5)
+  const [words, setWords] = useState<string[]>([
+    'React',
+    'Typescript',
+    'Web',
+    'Javascript',
+    'Vue',
+    'Unosquare',
+    'Array',
+    'String',
+    'Java',
+    'Localhost',
+    'Developer'
+  ])
+  const randomIndex: number = Math.floor(Math.random() * words.length)
+  const selectedInitialWord: string = words[randomIndex]
+  const [displayedWord, setDisplayedWord] = useState(
+    '_ '.repeat(selectedInitialWord.length)
+  )
+  const [guessedWord, setGuessedWord] = useState<string>('')
+  const inputRef = useRef<HTMLInputElement>(null);
 
-function App() {
-  const [game, setGame] = useState({
-    alphabet: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z'],
-    guessesLeft: 10
-    
-  })
-  const [words, setWords] = useState(['React', 'Typescript', 'Web', 'Javascript', 'Vue', 'Unosquare'])
-  const randomIndex = Math.floor(Math.random() * words.length);
-  const selectedWord =words[randomIndex]
-  const [displayedWord, setDisplayedWord] = useState('_ '.repeat(selectedWord.length));
+  function startNewGame () {
+    const newRandomIndex: number = Math.floor(Math.random() * words.length)
+    const newSecretWord: string = words[newRandomIndex]
+    //Set guesses left a 10
+    setDisplayedWord('_ '.repeat(newSecretWord.length))
+    console.log(newSecretWord)
+  }
+
+  function submitGuess (e: any) {
+    setGuess(guess - 1)
+    console.log(guessedWord)
+    //Poner condicion para cuando se llegue a  0
+  }
 
   return (
     <div className='flex flex-col items-center mt-10'>
-      <div className="text-5xl font-bold mb-10">Hangman Game</div>
-      <div className='flex flex-col md:flex-row w-full md:w-4/6 mx-auto justify-center items-center md:space-x-8'> 
-        <div className='flex-auto w-full md:w-2/5 md:order-1 md:mr-4 text-center md:text-left'> 
-          <div className='text-2xl font-bold mb-2 mt-5 md:mt-0'> 
-            Guesses left: {game.guessesLeft}
+      <div className='text-5xl font-bold mb-10'>Hangman Game</div>
+      <div className='flex flex-col md:flex-row w-full md:w-4/6 mx-auto justify-center items-center md:space-x-8'>
+        <div className='flex-auto w-full md:w-2/5 md:order-1 md:mr-4 text-center md:text-left'>
+          <div className='text-2xl font-bold mb-2 mt-5 md:mt-0'>
+            Guesses left: {guess}
           </div>
-          <div className='text-2xl font-bold mb-2 mt-5 md:mt-0'> 
-            Wrong Guesses: 
+          <div className='text-2xl font-bold mb-2 mt-5 md:mt-0'>
+            Wrong Guesses:
           </div>
-          <div className='text-1xl font-bold mb-2 mt-3 md:mt-0'> 
-            A S D F G H J K L Z X
-          </div>
+          <div className='text-1xl font-bold mb-2 mt-3 md:mt-0'>{}</div>
           <div className='flex justify-center md:justify-start mt-5'>
             <button
-              onClick={() => {}}
-              className="px-4 py-2 rounded-md font-bold text-sm bg-blue-500 text-white" 
+              onClick={startNewGame}
+              className='px-4 py-2 rounded-md font-bold text-sm bg-blue-500 text-white'
             >
               New Game
             </button>
           </div>
         </div>
-        <div className='flex-auto w-full md:w-2/8 md:order-2 md:mt-7 mt-4 flex-col text-center md:text-left'> 
-          <div className='text-6xl'>
-           {displayedWord}
-          </div>
-          <div className='text-3xl mt-4'> 
+        <div className='flex-auto w-full md:w-2/8 md:order-2 md:mt-7 mt-4 flex-col text-center md:text-left'>
+          <div className='text-6xl'>{displayedWord}</div>
+
+          <div className='text-3xl mt-4'>
             <input
-              type="text"
-              className="px-4 py-0 border mt-4 focus:outline-none text-sm rounded-md w-56 h-10"
-              placeholder="Type your guess"
+              type='text'
+              className='px-4 py-0 border mt-4 focus:outline-none text-sm rounded-md w-56 h-10'
+              placeholder='Type your guess'
+              value={guessedWord}
+              onChange={e => setGuessedWord(e.target.value)}
+              ref={inputRef} 
             />
             <button
-              onClick={() => {}}
-              className="px-2 py-2 rounded-md font-bold text-sm bg-blue-500 text-white mt-4" 
+              onClick={() => submitGuess(guessedWord)}
+              className='px-2 py-2 rounded-md font-bold text-sm bg-blue-500 text-white mt-4'
             >
               Submit
             </button>
@@ -56,7 +81,7 @@ function App() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
